3757672574d3280e922f3261cc51b1cd534c0955
Первушин <e.pervushin@talenttech.ru>
2025-05-31 11:26:18 +0300

    chore: удалить настройку задачи для sidekiq-cron из boot.rb

--- config/boot.rb
+++ config/boot.rb
@@ -7,18 +7,5 @@ loader.push_dir("#{__dir__}/../lib", namespace: Object)
 loader.push_dir("#{__dir__}/../models", namespace: Object)
 loader.push_dir("#{__dir__}/../lib/services", namespace: Object)
 loader.setup
-require 'sidekiq'
-require 'sidekiq/cron/job'
-
-# Настройка задачи для sidekiq-cron
-schedule = {
-  'CheckAllSnapshots' => {
-    'class' => 'SnapshotCheckAllWorker',
-    'cron' => '*/10 * * * *',
-    'description' => 'Check all snapshots every 10 minutes'
-  }
-}
-
-Sidekiq::Cron::Job.load_from_hash(schedule)
 
 loader.eager_load
