5fbc1a6e15f58a0cadebe609aab0c895ff7372c5
Первушин <e.pervushin@talenttech.ru>
2025-05-31 12:57:15 +0300

    test: исправить тесты для команды удаления снимка в боте

--- spec/bot/commands/delete_command_spec.rb
+++ spec/bot/commands/delete_command_spec.rb
@@ -3,21 +3,25 @@ require 'spec_helper'
 RSpec.describe Bot::Commands::DeleteCommand do
   describe '.call' do
     let(:bot) { double('bot', api: double('api', send_message: nil)) }
-    let(:message) { double('message', text: '/del 1', chat: double('chat', id: 123)) }
+    let(:chat_id) { 123 }
+    let!(:snapshot) { Snapshot.create(chat_id: chat_id) }
+    let(:message) { double('message', text: "/del #{snapshot.id}", chat: double('chat', id: chat_id)) }
 
     context 'when snapshot exists' do
-      let!(:snapshot) { Snapshot.create(chat_id: 123, id: 1) }
+      let!(:snapshot2) { Snapshot.create(chat_id: chat_id) }
 
       it 'deletes the snapshot and sends a success message' do
-        expect(bot.api).to receive(:send_message).with(chat_id: 123, text: 'Снимок с ID 1 удален.')
+        expect(bot.api).to receive(:send_message).with(chat_id: 123, text: "Снимок с ID #{snapshot.id} удален.")
         described_class.call(bot: bot, message: message)
-        expect(Snapshot[chat_id: 123, id: 1]).to be_nil
+        expect(Snapshot[snapshot2.id]).to be
+        expect(Snapshot[snapshot.id]).to be_nil
       end
     end
 
     context 'when snapshot does not exist' do
+      before { snapshot.destroy }
       it 'sends a message that snapshot is not found' do
-        expect(bot.api).to receive(:send_message).with(chat_id: 123, text: 'Снимок с ID 1 не найден.')
+        expect(bot.api).to receive(:send_message).with(chat_id: 123, text: "Снимок с ID #{snapshot.id} не найден.")
         described_class.call(bot: bot, message: message)
       end
     end
